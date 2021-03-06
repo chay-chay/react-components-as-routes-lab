e9cb195c737d9d45785921dfee7ad8ed9aed2b77
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-06 17:34:36 -0500

    commit tree 08c544575ec4beeccc967ba6733751b356621024

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '25 px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div>
+    <div className="navbar">
       {/*{code here}*/}
+    <ul>
+    <li><NavLink to='/'>Home</NavLink></li>
+        
+
+       
+    <li><NavLink to='/movies'>Movies</NavLink></li>
+     
+
+       
+    <li><NavLink to='/directors'>Directors</NavLink></li>
+       
+
+ 
+    <li><NavLink to='/actors'>Actors</NavLink></li>
+    </ul>   
+
+    
     </div>
   );
 };
